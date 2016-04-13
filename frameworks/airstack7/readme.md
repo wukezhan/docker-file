711ee4d231802c968277889ff982536e08951e08
wukezhan <wukezhan@gmail.com>
2016-04-13 14:20:26 +0800

    add airstack7

---
+++ frameworks/airstack7/readme.md
@@ -0,0 +1,22 @@
+```
+.
+|-- config
+|   `-- init
+|       `-- 02nginx
+|-- server
+|   `-- nginx
+|       `-- conf
+|           |-- addon.d
+|           |   |-- deny.conf
+|           |   `-- static.conf
+|           |-- misc.d
+|           |   |-- 01mime.conf
+|           |   |-- 02logs.conf
+|           |   |-- 03misc.conf
+|           |   `-- 04gzip.conf
+|           |-- site.d
+|           |   `-- default.conf
+|           `-- nginx.conf
+|-- Dockerfile
+`-- readme.md
+```
---
+++ frameworks/airstack7/server/php/script/build_env.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * 
+ * @author wukezhan<wukezhan@gmail.com>
+ * 2014-11-02 12:01
+ * 
+ */
+$conf = '/server/etc/php/env.conf';
+if(!file_exists($conf)){
+    $ignores = array(
+        'LESSOPEN' => 1,
+        'SHLVL' => 1,
+        'LS_COLORS' => 1,
+        'PWD' => 1,
+        'OLDPWD' => 1,
+        'DEBIAN_FRONTEND' => 1,
+        'LESSCLOSE' => 1,
+        'PATH' => 1,
+        'TERM' => 1,
+        '_' => 1,
+    );
+    ob_start();
+    system('env', $env);
+    $env = trim(ob_get_contents());
+    ob_end_clean();
+    $env = explode("\n", $env);
+    $content = array('[default]');
+    foreach($env as $line){
+        $line = explode("=", $line, 2);
+        if(!isset($ignores[$line[0]])){
+            $content[$line[0]] = "env[{$line[0]}] = \${$line[0]}";
+            $ignores[$line[0]] = 1;
+        }
+    }
+    $content = implode("\n", $content);
+    file_put_contents($conf, $content);
+}
